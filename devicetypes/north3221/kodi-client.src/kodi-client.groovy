2e1cff1bd8482ff9e085547f74a5ebdc3f0be774
north3221 <north3221@gmail.com>
2017-01-22 19:50:05 +0000

    Trying to handle preference conversion to compare

--- devicetypes/north3221/kodi-client.src/kodi-client.groovy
+++ devicetypes/north3221/kodi-client.src/kodi-client.groovy
@@ -192,7 +192,7 @@ def parse(evt) {
                 //movieLabel = inputMovieLabel.toLowerCase().toSet()
             }
             log.info "Movie list = " + movieLabel
-            movieLabel = movieLabel.toSet()
+            movieLabel = movieLabel.split(',').toSet()
             log.info "Movie list = " + movieLabel
             //Set sport label list
             def sportLabel = defaultSportLabels.toSet()
