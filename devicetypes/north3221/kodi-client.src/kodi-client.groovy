2702dff2ee009bcafaa59c819afb30cf3348b551
north3221 <north3221@gmail.com>
2017-01-22 20:09:58 +0000

    Trying to handle preference conversion to compare

--- devicetypes/north3221/kodi-client.src/kodi-client.groovy
+++ devicetypes/north3221/kodi-client.src/kodi-client.groovy
@@ -24,7 +24,7 @@ def getDefaultMovieLabels() {
     if (inputMovieLabel){
         returnList = inputMovieLabel
     }
-    returnList.toLowerCase().split(',')
+    returnList.toLowerCase()
     return returnList
 }
 def getDefaultSportLabels() {
