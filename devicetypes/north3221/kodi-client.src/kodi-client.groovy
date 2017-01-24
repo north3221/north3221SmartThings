75df5897bc9f93031f5f2ee90df1277eb4c6e96b
north3221 <north3221@gmail.com>
2017-01-24 00:09:10 +0000

    added logging to category unknown check

--- devicetypes/north3221/kodi-client.src/kodi-client.groovy
+++ devicetypes/north3221/kodi-client.src/kodi-client.groovy
@@ -194,8 +194,8 @@ def parse(evt) {
             def runtime = slurper.result.item.runtime
             def plot = slurper.result.item.plot
             //Set movie label list
-            def movieLabel = defaultMovieLabels.split(',')
-            movieLabel = ["movie", "cinema", "film"]
+            def movieLabel = defaultMovieLabels.split(',').toSet()
+            //movieLabel = ["movie", "cinema", "film"]
             //Set sport label list
             def sportLabel = defaultSportLabels.split(',')
             //Set tv label list
