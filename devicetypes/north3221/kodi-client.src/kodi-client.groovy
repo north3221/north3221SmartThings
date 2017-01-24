267cbe56d58d37c000073696d527a5609d13e787
north3221 <north3221@gmail.com>
2017-01-24 01:23:41 +0000

    again

--- devicetypes/north3221/kodi-client.src/kodi-client.groovy
+++ devicetypes/north3221/kodi-client.src/kodi-client.groovy
@@ -194,8 +194,8 @@ def parse(evt) {
             def runtime = slurper.result.item.runtime
             def plot = slurper.result.item.plot
             //Set movie label list
-            def movieLabel = new JsonSlurper().parseText(defaultMovieLabels)
-            //movieLabel = ["movie", "cinema", "film"]
+            //def movieLabel = new JsonSlurper().parseText(defaultMovieLabels)
+            def movieLabel = ["movie", "cinema", "film"]
             //Set sport label list
             def sportLabel = defaultSportLabels.split(',')
             //Set tv label list
