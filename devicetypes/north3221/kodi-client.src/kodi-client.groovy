8b0803883619d4013caed605709300f7f1ca0a09
north3221 <north3221@gmail.com>
2017-01-24 19:11:02 +0000

    trying stuff

--- devicetypes/north3221/kodi-client.src/kodi-client.groovy
+++ devicetypes/north3221/kodi-client.src/kodi-client.groovy
@@ -197,7 +197,7 @@ def parseNowPlaying(msgBody){
         log.info "unknown type so checking label (" + label + ") contains Movie (" + movieLabels + ") or Sport (" + sportLabels + ") or TV Show (" + tvLabels + ")"
         //Check labels
         movieLabels.any {
-            log.info "Checking if (" + label.toLowerCase() + ") contains " + it
+            log.info "Checking if (" + label.toLowerCase() + ") contains (" + it + ")"
             if (label.toString().toLowerCase().contains(it.toString())) {
                 log.info "contains (" + it + ") - length =" + it.length()
                 category = "Movie"
