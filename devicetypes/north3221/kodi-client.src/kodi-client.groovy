7ff7d6a8a5edebebd61b3c4372ea5edeb67478d3
north3221 <north3221@gmail.com>
2017-01-24 00:13:10 +0000

    fixing the type conversion as the lables check isnt working

--- devicetypes/north3221/kodi-client.src/kodi-client.groovy
+++ devicetypes/north3221/kodi-client.src/kodi-client.groovy
@@ -194,7 +194,7 @@ def parse(evt) {
             def runtime = slurper.result.item.runtime
             def plot = slurper.result.item.plot
             //Set movie label list
-            def movieLabel = defaultMovieLabels.split(',').toListString()
+            def movieLabel = defaultMovieLabels.split(',').toString()
             //movieLabel = ["movie", "cinema", "film"]
             //Set sport label list
             def sportLabel = defaultSportLabels.split(',')
