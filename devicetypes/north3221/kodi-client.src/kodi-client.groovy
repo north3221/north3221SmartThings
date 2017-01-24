73ca3f4f0dddf0910e594c334769b7af3c4673f4
north3221 <north3221@gmail.com>
2017-01-24 13:12:33 +0000

    trying stuff

--- devicetypes/north3221/kodi-client.src/kodi-client.groovy
+++ devicetypes/north3221/kodi-client.src/kodi-client.groovy
@@ -237,7 +237,7 @@ def parse(evt) {
             //Set sport label list
             def sportLabel = defaultSportLabels.split(',')
             //Set tv label list
-            List<String> tvShowLabel = defaultTVLabels.split(',')
+            def tvShowLabel = defaultTVLabels.split(',')
             //log.info "movie class is " + movieLabel.class
             //log.info "TV class is " + tvShowLabel.class
             //log.info "movie size = " + movieLabel.size()
