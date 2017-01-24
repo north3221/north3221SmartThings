af27325c0c7bfec2c389d2a40c3cebf84654973e
north3221 <north3221@gmail.com>
2017-01-24 00:26:05 +0000

    still trying to fix

--- devicetypes/north3221/kodi-client.src/kodi-client.groovy
+++ devicetypes/north3221/kodi-client.src/kodi-client.groovy
@@ -194,12 +194,12 @@ def parse(evt) {
             def runtime = slurper.result.item.runtime
             def plot = slurper.result.item.plot
             //Set movie label list
-            def movieLabel = defaultMovieLabels.split(',').toList()
+            def movieLabel = defaultMovieLabels.split(',').toString().toSet()
             //movieLabel = ["movie", "cinema", "film"]
             //Set sport label list
-            def sportLabel = defaultSportLabels.split(',').toList()
+            def sportLabel = defaultSportLabels.split(',').toString().toSet()
             //Set tv label list
-            def tvShowLabel = defaultTVLabels.split(',').toList()
+            def tvShowLabel = defaultTVLabels.split(',').toString().toSet()
             //Set min runtime to be a movie
             def minMovieRuntime = defaultMinMovieRuntime
 
