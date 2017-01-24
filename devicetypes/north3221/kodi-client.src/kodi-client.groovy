3cf9d85267531681b12290f49d6a040bdc46e638
north3221 <north3221@gmail.com>
2017-01-24 00:20:05 +0000

    fixed the issue where only the first in list can match (needed toString())

--- devicetypes/north3221/kodi-client.src/kodi-client.groovy
+++ devicetypes/north3221/kodi-client.src/kodi-client.groovy
@@ -194,7 +194,7 @@ def parse(evt) {
             def runtime = slurper.result.item.runtime
             def plot = slurper.result.item.plot
             //Set movie label list
-            def movieLabel = defaultMovieLabels.split(',').toString()
+            def movieLabel = defaultMovieLabels.split(',').tostring()
             //movieLabel = ["movie", "cinema", "film"]
             //Set sport label list
             def sportLabel = defaultSportLabels.split(',').toString()
