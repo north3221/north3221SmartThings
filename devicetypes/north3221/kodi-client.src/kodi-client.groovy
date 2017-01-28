43d7bc92cdfbc17b630e15830eec453e7f53f66b
north3221 <north3221@gmail.com>
2017-01-28 20:54:33 +0000

    fixing ui

--- devicetypes/north3221/kodi-client.src/kodi-client.groovy
+++ devicetypes/north3221/kodi-client.src/kodi-client.groovy
@@ -129,8 +129,8 @@ metadata {
 
         standardTile("stop", "device.status", width: 1, height: 1) {
             state "stopped", label:'', action:"music Player.stop", icon:"st.sonos.stop-btn", backgroundColor:tileWhite, defaultState: true
-            state "playing", label:'', action:"music Player.stop", icon:"st.sonos.stop-btn", backgroundColor:tileRed
-            state "paused", label:'', action:"music Player.stop", icon:"st.sonos.stop-btn", backgroundColor:tileRed
+            //state "playing", label:'', action:"music Player.stop", icon:"st.sonos.stop-btn", backgroundColor:tileRed
+            //state "paused", label:'', action:"music Player.stop", icon:"st.sonos.stop-btn", backgroundColor:tileRed
         }
 
         standardTile("shutdown", "device.shutdown", width: 1, height: 1, decoration: "ring") {
