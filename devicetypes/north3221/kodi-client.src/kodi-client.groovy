7ae6d434de1fcddca5be9e0d6e32f0e9ad2c275c
north3221 <north3221@gmail.com>
2017-01-28 19:24:12 +0000

    fix shutdown as quit label

--- devicetypes/north3221/kodi-client.src/kodi-client.groovy
+++ devicetypes/north3221/kodi-client.src/kodi-client.groovy
@@ -168,7 +168,7 @@ metadata {
 
         standardTile("info", "device.info", width: 1, height: 1) {
             //state "on", label:'', action:"info", icon:"https://cdn2.iconfinder.com/data/icons/iconza/iconza_32x32_808080/info.png", backgroundColor:tileWhite, defaultState: true
-            state "info", label:'', action:"info", icon:"http://cdn.mysitemyway.com/etc-mysitemyway/icons/legacy-previews/icons-256/3d-transparent-glass-icons-alphanumeric/068021-3d-transparent-glass-icon-alphanumeric-information2-ps.png", backgroundColor:tileWhite, defaultState: true
+            state "info", label:'', action:"info", icon:"http://cdn.mysitemyway.com/etc-mysitemyway/icons/legacy-previews/icons/glossy-silver-icons-alphanumeric/070981-glossy-silver-icon-alphanumeric-information2-ps.png", backgroundColor:tileWhite, defaultState: true
         }
 
         standardTile("2x1", "device.status", width: 2, height: 1, decoration: "flat") {
