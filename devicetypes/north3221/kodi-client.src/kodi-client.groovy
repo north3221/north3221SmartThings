a708c95372997d3e7099ea83b25797845d2fdd6b
north3221 <north3221@gmail.com>
2017-01-28 15:24:45 +0000

    fix shutdown as quit label

--- devicetypes/north3221/kodi-client.src/kodi-client.groovy
+++ devicetypes/north3221/kodi-client.src/kodi-client.groovy
@@ -335,6 +335,7 @@ def executeAction(action) {
 }
 
 def push() {
+    log.debug "Current shutdown typpe = " + "${currentShutdownType}"
     executeAction("select")
 }
 //Play pause for action button
