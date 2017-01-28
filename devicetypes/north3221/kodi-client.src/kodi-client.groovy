5162eba0d4b0b36d72774385385acd135e4cf015
north3221 <north3221@gmail.com>
2017-01-28 15:00:03 +0000

    fix shutdown as quit label

--- devicetypes/north3221/kodi-client.src/kodi-client.groovy
+++ devicetypes/north3221/kodi-client.src/kodi-client.groovy
@@ -134,8 +134,8 @@ metadata {
             state "paused", label:'', action:"music Player.stop", icon:"st.sonos.stop-btn", backgroundColor:tileRed
         }
 
-        standardTile("shutdown", "device.status", width: 1, height: 1) {
-            state "playing", label:"${shutdownType}", action:"shutdown", icon:"st.samsung.da.RC_ic_power", backgroundColor:tileRed, defaultState: true
+        standardTile("shutdown", "device.shutdown", width: 1, height: 1) {
+            state "playing", label:"device.shutdownType", action:"shutdown", icon:"st.samsung.da.RC_ic_power", backgroundColor:tileRed, defaultState: true
             state "shutdown", label:'', action:"shutdown", icon:"st.samsung.da.RC_ic_power", backgroundColor:tileWhite
         }
 
