7ce09c851512fd193df093e3cbbe6a824ca1eec8
north3221 <north3221@gmail.com>
2017-02-03 17:52:25 +0000

    Added glymphs theme

--- devicetypes/north3221/kodi-client.src/kodi-client.groovy
+++ devicetypes/north3221/kodi-client.src/kodi-client.groovy
@@ -255,7 +255,7 @@ log.debug "Initialised"
 }
 
 def updated() {
-    log.debug "updated - Theme = " inputTheme
+    log.debug "updated - Theme = $inputTheme"
     sendEvent(name: "updateUI", value: inputTheme);
 }
 
